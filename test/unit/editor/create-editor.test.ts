b89a0cf72346b1bc50e34fb77d2a2e9355e00bc9
qianfengg <fred_gqf0221@163.com>
2021-02-24 15:52:25 +0800

    fix: 修复textSelector中带标签的内容不能带入编辑器内

--- test/unit/editor/create-editor.test.ts
+++ test/unit/editor/create-editor.test.ts
@@ -21,3 +21,11 @@ test('一个页面创建多个编辑器实例', () => {
     expect(editor1.id).not.toBeNull()
     expect(editor2.id).not.toBeNull()
 })
+
+test('创建一个编辑器实例，toolbar 和 text 分离，且文本区域带标签的内容代入编辑区域', () => {
+    const editor = createEditor(document, 'div1', 'div2', {}, `<p>测试下</p>`)
+    expect(editor.id).not.toBeNull()
+    setTimeout(() => {
+        expect(editor.txt.text()).toBe('测试下')
+    })
+})
